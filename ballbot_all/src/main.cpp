860009101f272955887e4cca4828cbd9cc82a956
TEINTURIER Gabin <gabin@LAPTOP-6NV1ITJI.>
2025-02-04 14:24:54 +0100

    Voila le fameux code Mathosmls

--- ballbot_all/src/main.cpp
+++ ballbot_all/src/main.cpp
@@ -3,8 +3,10 @@
 #include <PciListenerImp.h>
 #include "imu.hpp"
 #include "pid.hpp"          
- #include "CytronMotorDriver.h"
- #include <Arduino.h>
+#include "CytronMotorDriver.h"
+#include <Arduino.h>
+#include <math.h>
+
 //---------------------------------
 //ENCODEURS
 //---------------------------------
@@ -43,41 +45,52 @@ void printEncoderInfo() {
         Serial.println(encoders[j].getVelocity());
     }
 }
+
 //----------------------------------------
 //IMU
 //----------------------------------------
-
-IMU myIMU(500,2);
+IMU myIMU(1000,4);
 
 //----------------------------------------
-//PID
+//PID simple
 //----------------------------------------
-double Kp_pitch = 2.0, Ki_pitch = 0.0, Kd_pitch = 0.0;
-double Kp_roll = -2.0, Ki_roll = 0.0, Kd_roll = 0.0;
-double setpoint_pitch = 0.;        // Angle cible calculé par la boucle externe
-double setpoint_roll = 0.;        // Angle cible calculé par la boucle externe
-double roll, pitch, vx, vy; // Entrée et sortie de la boucle interne
-MyPID pid_pitch(Kp_pitch, Ki_pitch, Kd_pitch, &pitch, &vx, &setpoint_pitch);
-MyPID pid_roll(Kp_roll, Ki_roll, Kd_roll, &roll, &vy, &setpoint_roll);
+double Kp = 1000.0, Ki = 0.0, Kd = 100.0;
+double pi = 3.14159265358979;
+double setpoint = 0.;        // Angle cible calculé par la boucle externe
+double roll, pitch, psi, alpha, v; // Entrée et sortie de la boucle interne
+MyPID pid_alpha(Kp, Ki, Kd, &alpha, &v, &setpoint);
+bool boost[3] = {false, false, false};
 
+double get_motor_speed(int numero, double psi, double v) 
+{
+  double v_motor = constrain(abs(v)*sin(psi + (1 - numero)*(2./3.)*pi), -255, 255);
+  if (abs(v_motor) < 30 && !boost[numero]) {
+    boost[numero] = true;
+    return 150*v_motor/abs(v_motor);
+  }
+  else if (abs(v_motor) < 30 && boost[numero]) {
+    return 30*v_motor/abs(v_motor);
+  }
+  else {
+    boost[numero] = false;
+    return v_motor;
+  }
+}
 
 void computePID()
 {
     roll = myIMU.get_roll_rad();
     pitch = myIMU.get_pitch_rad();
-    pid_pitch.Compute();
-    pid_roll.Compute();
-    vx = constrain(vx, -2, 2);  
-    vy = constrain(vy, -2, 2); 
+    alpha = asin(sqrt(pow(sin(roll), 2) + pow(sin(pitch), 2)));
+    pid_alpha.Compute();
 }
 
 //----------------------------------------
 //Motors
 //----------------------------------------
-
-CytronMD motor1(PWM_DIR, 10, 12);  // PWM 1 = Pin 3, DIR 1 = Pin 4.
-CytronMD motor2(PWM_DIR, 9, 8); // PWM 2 = Pin 9, DIR 2 = Pin 10.
-CytronMD motor3(PWM_DIR, 11, 13); // PWM 2 = Pin 9, DIR 2 = Pin 10.
+CytronMD motor1(PWM_DIR, 12, 10);  // PWM 1 = Pin 12, DIR 1 = Pin 10.
+CytronMD motor2(PWM_DIR, 8, 9); // PWM 2 = Pin 8, DIR 2 = Pin 9.
+CytronMD motor3(PWM_DIR, 7, 11); // PWM 3 = Pin 7, DIR 3 = Pin 11.
 
 //---------------------------------------------------------------------------
 
@@ -125,18 +138,35 @@ void loop() {
     // motor3.setSpeed(200);   // Motor 1 runs forward at 50% speed.
     Serial.println("boucle");
     // delay(10000);
-    if (i == 50) {
+    psi = atan2(sin(roll), sin(pitch));
+    motor1.setSpeed(get_motor_speed(1, psi, v));
+    motor2.setSpeed(get_motor_speed(2, psi, v));
+    motor3.setSpeed(get_motor_speed(3, psi, v));
+
+    if (i == 400) {
         // printEncoderInfo();
         myIMU.printAll();
-        myIMU.printAngle();
-        // Serial.print("PID vx | vy: ");
-        // Serial.print(vx);
-        // Serial.print("\t");
-        // Serial.println(vy);
+        Serial.println("==================================================");
+        Serial.print("PID : V = ");
+        Serial.print(abs(v));
+        Serial.print(", psi = ");
+        Serial.print(psi);
+        Serial.print(", alpha = ");
+        Serial.println(alpha);
+
+        Serial.println("Vitesses moteurs : ");
+        Serial.print("v1 = ");
+        Serial.println(get_motor_speed(1, psi, v));
+        Serial.print("v2 = ");
+        Serial.println(get_motor_speed(2, psi, v));
+        Serial.print("v3 = ");
+        Serial.println(get_motor_speed(3, psi, v));
+        Serial.println("==================================================");
         i = 0;
     } else {
         i++;
     }
+    delay(10);
 }
 
 
